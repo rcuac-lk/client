d059908b9a762063d6b92008289c6edbd745d4a2
Kushantha <kushantha018@gmail.com>
2024-10-22 01:56:25 +0530

    Update BASE_URL

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@
 import api from "./api";
 import authHeader from "./auth-header";
 
-// const API_URL = "http://localhost:8080/api/";
+// const API_URL = process.env.BASE_URL;
 const API_URL = "https://server.rcuac.lk/api/";
 
 // const getPublicContent = () => {

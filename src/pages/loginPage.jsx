aaed10e55d7ea5418f181cb8066337ea70a3b83c
Kushantha <kushantha018@gmail.com>
2024-12-07 15:33:15 +0530

    Add Error Handling to login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -6,6 +6,7 @@ const LoginPage = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
 
     const handleSubmit = async (event) => {
@@ -25,10 +26,16 @@ const LoginPage = () => {
                     }
                 },
                 (error) => {
+                    if (error.response && error.response.data && error.response.data.message) {
+                        setErrorMessage(error.response.data.message);
+                    } else {
+                        setErrorMessage('An unexpected error occurred. Please try again.');
+                    }
                     console.log(error);
                 }
             );
         } catch (error) {
+            setErrorMessage('An unexpected error occurred. Please try again later.');
             console.log(error);
         }
     };
@@ -37,6 +44,9 @@ const LoginPage = () => {
         <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center">
             <h1 className="text-3xl font-bold text-white text-center mb-5">Login</h1>
             <form onSubmit={handleSubmit} class="max-w-sm w-full bg-gray-800 p-8 rounded-lg">
+                {errorMessage && (
+                    <div className="text-red-500 text-sm mb-5">{errorMessage}</div>
+                )}
                 {/* <div class="mb-5">
                     <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" class="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@example.com" required />

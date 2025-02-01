f33d05029ab71262389519c6b6ea962d6ee7d9fe
Kushantha <kushantha018@gmail.com>
2025-02-01 15:08:04 +0530

    Added reject button

--- src/components/pendingApprovals.jsx
+++ src/components/pendingApprovals.jsx
@@ -4,6 +4,7 @@ import UserService from '../services/user.service';
 const PendingApprovalsComponent = () => {
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+    const [isRejectModalOpen, setIsRejectModalOpen] = useState(false);
     const [users, setUsers] = useState([]);
     const [selectedUser, setSelectedUser] = useState(null); // To store the selected user's data
 
@@ -32,6 +33,16 @@ const PendingApprovalsComponent = () => {
         setSelectedUser(null);
     };
 
+    const openRejectModal = (user) => {
+        setSelectedUser(user);
+        setIsRejectModalOpen(true);
+    };
+
+    const closeRejectModal = () => {
+        setIsRejectModalOpen(false);
+        setSelectedUser(null);
+    };
+
     const handleApprove = async () => {
         try {
             await UserService.approveUser(selectedUser.UserID);
@@ -39,6 +50,16 @@ const PendingApprovalsComponent = () => {
             notApprovedUsers();
         } catch (error) {
             console.error("Error approving user:", error);
+        }
+    };
+
+    const handleReject = async () => {
+        try {
+            await UserService.rejectUser(selectedUser.UserID);
+            closeRejectModal();
+            notApprovedUsers();
+        } catch (error) {
+            console.error("Error rejecting user:", error);
         }
     };
 
@@ -92,9 +113,12 @@ const PendingApprovalsComponent = () => {
                                     </div>
                                 </th>
                                 <td className="px-6 py-4">{user.Role}</td>
-                                <td className="px-6 py-4">
+                                <td className="px-6 py-4 flex">
                                     <div className="flex items-center">
                                         <button type="button" onClick={() => openConfirmModal(user)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Approve</button>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <button type="button" onClick={() => openRejectModal(user)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Reject</button>
                                     </div>
                                 </td>
                                 <td className="px-6 py-4">
@@ -125,6 +149,32 @@ const PendingApprovalsComponent = () => {
                                         Yes, approve
                                     </button>
                                     <button type="button" onClick={closeConfirmModal} className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {/* Reject Modal */}
+                {isRejectModalOpen && (
+                    <div id="reject-modal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div className="relative w-full max-w-md max-h-full">
+                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <button type="button" onClick={closeRejectModal} className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                                <div className="p-4 md:p-5 text-center">
+                                    <svg className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                    </svg>
+                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to reject this user?</h3>
+                                    <button type="button" onClick={handleReject} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Yes, reject
+                                    </button>
+                                    <button type="button" onClick={closeRejectModal} className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
                                 </div>
                             </div>
                         </div>

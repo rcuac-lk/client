1c573a5898c6eed5383e0f4e38ba91462a7fbd6c
Kushantha <kushantha018@gmail.com>
2025-02-01 15:09:08 +0530

    Fix signout issue

--- src/pages/coachDashboard.jsx
+++ src/pages/coachDashboard.jsx
@@ -176,7 +176,7 @@ const CoachDashboard = () => {
                       </a>
                     </li>
                     <li>
-                      <a
+                      <a onClick={logoutHandler}
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                         role="menuitem"

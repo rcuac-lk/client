795b05174cfd9bddc12c85b2d1c615501a578241
Kushantha <kushantha018@gmail.com>
2025-02-05 19:07:11 +0530

    Add attendance section

---
+++ src/components/attendance.jsx
@@ -0,0 +1,7 @@
+const Attendance = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Attendance;
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,6 +4,7 @@ import AuthService from "../services/auth.service";
 import UserService from "../services/user.service";
 import UserListComponent from "../components/userList";
 import PendingApprovalsComponent from "../components/pendingApprovals";
+import Attendance from "../components/attendance";
 
 const Dashboard = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -309,7 +310,27 @@ const handleInputChange = (e) => {
                 <span className="ms-3">Pending Approvals</span>
               </a>
             </li>
+            <li>
+              <a
+                href="#"
+                onClick={() => handleMenuClick('attendance')}
+                className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
+              >
+                <svg
+                  className="w-5 h-5 transition duration-75 text-gray-400 group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Attendance</span>
+              </a>
+            </li>
             {/* Other items here */}
+
           </ul>
           <div className="mt-auto">
             <ul className="space-y-2 font-medium">
@@ -358,6 +379,9 @@ const handleInputChange = (e) => {
           )}
           {selectedSection === 'pendingApprovals' && (
             <PendingApprovalsComponent /> // Replace with actual Pending Approvals component or content
+          )}
+          {selectedSection === 'attendance' && (
+            <Attendance />
           )}
         </div>
       </div>

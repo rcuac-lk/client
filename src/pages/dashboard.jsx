c7cecc83e6565e3f25be94e2e17eaa598562b64e
Kushantha <kushantha018@gmail.com>
2025-02-06 16:42:28 +0530

    Remove dashboard option

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -5,6 +5,7 @@ import UserService from "../services/user.service";
 import UserListComponent from "../components/userList";
 import PendingApprovalsComponent from "../components/pendingApprovals";
 import Attendance from "../components/attendance";
+import Timing from "../components/timing";
 
 const Dashboard = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -213,15 +214,6 @@ const handleInputChange = (e) => {
                         href="#"
                         className="block px-4 py-2 text-sm text-gray-300 hover:bg-gray-600 hover:text-white"
                         role="menuitem"
-                      >
-                        Dashboard
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-300 hover:bg-gray-600 hover:text-white"
-                        role="menuitem"
                         onClick={() => openEditModal(userDetails.id)}
                       >
                         Profile
@@ -329,6 +321,25 @@ const handleInputChange = (e) => {
                 <span className="ms-3">Attendance</span>
               </a>
             </li>
+            <li>
+              <a
+                href="#"
+                onClick={() => handleMenuClick('timing')}
+                className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
+              >
+                <svg
+                  className="w-5 h-5 transition duration-75 text-gray-400 group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Timing</span>
+              </a>
+            </li>
             {/* Other items here */}
 
           </ul>
@@ -382,6 +393,9 @@ const handleInputChange = (e) => {
           )}
           {selectedSection === 'attendance' && (
             <Attendance />
+          )}
+          {selectedSection === 'timing' && (
+            <Timing />
           )}
         </div>
       </div>

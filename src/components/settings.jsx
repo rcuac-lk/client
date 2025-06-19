7d776dbe05fa7477fd12111bd307678bb912d303
Kushantha <kushantha018@gmail.com>
2025-06-19 18:41:36 +0530

    Add settings component

---
+++ src/components/settings.jsx
@@ -0,0 +1,11 @@
+const Settings = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-16">
+                <h1>Settings</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
